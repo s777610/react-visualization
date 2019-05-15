3f529ec7ba2c70abdda72eb3c5622ef3bd2defb9
s777610 <s777610@gmail.com>
2019-05-15 03:44:58 -0700

    setup react-interactive chart

---
+++ src/components/layout/HeadBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeadBar = () => {
+  return (
+    <div className="bar">
+      <div>CryptoBash </div>
+      <div />
+      <div>Dashboard </div>
+      <div>Settings </div>
+    </div>
+  );
+};
+
+export default HeadBar;
